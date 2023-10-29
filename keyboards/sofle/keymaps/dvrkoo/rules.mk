e68b740fd50da1f794f737a3d56f485a4f6db8cd
dvrko <nico.marini@hotmail.it>
2023-10-29 13:24:33 +0100

    my keymap

---
+++ keyboards/sofle/keymaps/dvrkoo/rules.mk
@@ -0,0 +1,10 @@
+
+OLED_ENABLE = yes
+LTO_ENABLE = yes
+ENCODER_ENABLE = yes
+#CONSOLE_ENABLE = no Includes debugging info which idgaf
+EXTRAKEY_ENABLE = yes
+WPM_ENABLE = yes
+#VIA_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
---
+++ keyboards/sofle/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#define VIAL_KEYBOARD_UID {0xD7, 0x5A, 0xBF, 0xDC, 0x4B, 0xE8, 0xA5, 0xCC}
